c6167e32a02c9691ae6d4979e07eecaaf3d6f169
Jonas Enge <jonas.enge@gmail.com>
2018-08-18 13:20:19 +0200

    Genesis commit

---
+++ tst.js
@@ -0,0 +1,23 @@
+(async () => {
+  const { readFileSync } = require('fs')
+  const openIdClient = require('./src/index')
+
+  const clientOptions = {
+    url: 'https://oidc-ver1.difi.no/',
+    cert: readFileSync('./src/data/public.pem', 'utf-8'),
+    privateKey: readFileSync('./src/data/private.key', 'utf-8'),
+    /* privateKeyPassphrase: 'password', // uncomment if privateKey is encrypted */
+    issuer: 'Din-id',
+    scope: 'global/kontaktinformasjon.read'
+  }
+
+  try {
+    const client = await openIdClient(clientOptions)
+    const { token: { access_token: token } } = client.getConfig()
+    const payload = { personidentifikatorer: [ '23079422568' ] }
+    const data = await client.getData({ url: clientOptions.url + 'kontaktinfo-oauth2-server/rest/v1/personer', token, payload })
+    console.log(JSON.stringify(data, null, 2))
+  } catch (error) {
+    console.error(error)
+  }
+})()
